4e1f5656dc9ca3b4606ec6996d1e365afe7dc220
labib3013 <79435333+labib3013@users.noreply.github.com>
2022-09-23 08:26:09 +0700

    commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>{{title}}</h1>
+  <Modal :header="header" :text="text"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld, Modal
+  },
+  data(){
+    return{
+      title:'Vue App',
+      header:'Mobil',
+      text:'Motor'
+    }
+  },
+  methods:{
+    handleClick(){
+      this.$refs.name1.classList.add('active');
+      this.$refs.name1.focus();
+      console.log(this.$refs.name1);
+  }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
